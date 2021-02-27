282ba8573945ecf81952e35f4d28c5e0a955a9b5
ike-albert <ikeoluwa.albert@gmail.com>
2021-02-27 00:12:53 -0600

    Finished Assignment 2 required stories, tweaked some ui/ux features, and added landscape orientation

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -2,19 +2,25 @@ package com.example.flixter.adapters;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -54,7 +60,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-
+        RelativeLayout container; //This is a relative layout that was created in the xml file
+        //as a way of being able to reference the entire row(everything concerning 1 movie-
+        // title, image, description)
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -65,14 +73,46 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+
+            // Set it to the backdrop url if it's in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            }else {
+                //Set imageUrl to posterPath if image is in portrait orientation.
+                imageUrl = movie.getPosterPath();
+            }
+
+
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
             //Making use of Glide cause there's no way to directly load remote
             // images from android studio yet
+
+            //Whenever the user clicks on a movie title, we want to show them more details about the movie
+            //1.  we need to create a way to know when the click on a movie title first
+            //- Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener()  {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    //A toast is like an alternative for debugging. THis one in particular pops up
+                    // a text, the movie name,  when the title is clicked on
+
+                    //2. Navigate to a new activity when title is clicked
+                    //Intent- First parameter is the context; second is the class of the activity to launch
+                    Intent i = new Intent(context, DetailActivity.class);
+//                  i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i); //Brings up second activity
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,20 +3,35 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
+    String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // Empty constructor needed by the Parceler library
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
+        backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");//Path to movie's poster
         title = jsonObject.getString("title"); //movie's title
         overview = jsonObject.getString("overview");//movie's description
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+
+
     }
 
     public static List<Movie> fromJsonArray(JSONArray jsonArray) throws JSONException {
@@ -40,6 +55,10 @@ public class Movie {
 
     }
 
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342%s",backdropPath);
+    }
+
     public String getTitle(){
         return title;
     }
@@ -47,4 +66,8 @@ public class Movie {
     public String getOverview(){
         return overview;
     }
+
+    public double getRating(){ return rating; }
+
+    public int getMovieId(){ return movieId; }
 }
